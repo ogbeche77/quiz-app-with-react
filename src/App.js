c38b951474cef10292fecc738942ad3fef3d6e3c
ogbeche77 <ogbeche77@yahoo.com>
2020-05-25 14:01:42 +0200

    Auth0 synthaxes

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './assets/style.css';
+import quizService from './quizService';
+import QuestionBox from './components/QuestionBox';
+import Result from './components/Result';
+import NavBar from "./components1/NavBar";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components1/Profile";
+import history from "./utils/history";
+import PrivateRoute from "./components1/PrivateRoute";
+
+
+class App extends Component {
+
+    state = {
+        questionBank: [],
+        score: 0,
+        responses: 0
+    };
+    getQuestions = ()=>{
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            })
+        })
+    }
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score:this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        });
+
+    }
+    playAgain = ()=> {
+        this.getQuestions();
+        this.setState({
+            score:0,
+            responses:0
+        });
+    }
+
+
+    componentDidMount() {
+        this.getQuestions();
+    }
+
+
+    render() {
+        return(
+           <div className="container">
+           <Router history={history}>
+           <header>
+           <NavBar/>
+           
+           </header>
+           <Switch>
+          <Route  path="/profile"   />
+          <PrivateRoute exact path="/" component={Profile} />
+        </Switch>
+            <div className="title">General Knowledge Quiz</div>
+           <ol className="myList">{this.state.questionBank.length > 0 &&
+                this.state.responses < 5 && this.state.questionBank.map(({
+                question,answers,correct, questionId }) => 
+                 
+                <li className="MyListClass"> <QuestionBox question={question} options={answers} key={questionId}
+                  selected={answer => this.computeAnswer(answer, correct)}/> </li> )} </ol>
+                  {this.state.responses === 5 ? (<Result score={this.state.score}
+                     playAgain={this.playAgain}/>): null }
+
+                     </Router>  
+            </div>
+
+
+        );
+        }
+}
+export default App
