0a11f24fb5aff9e255a94162288057e95fe1594e
ogbeche77 <ogbeche77@yahoo.com>
2020-05-26 11:02:40 +0200

    Updates

---
+++ precache-manifest.a205f8989977ceec40d3466a9fa48a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf27d6b1ffeb2bfb6df1b75f0717208",
+    "url": "/quiz-app-with-react/index.html"
+  },
+  {
+    "revision": "2493c475e4a94bf60a91",
+    "url": "/quiz-app-with-react/static/css/main.925eefbe.chunk.css"
+  },
+  {
+    "revision": "7b3fccfa96d377c29288",
+    "url": "/quiz-app-with-react/static/js/2.cd93a8ba.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/quiz-app-with-react/static/js/2.cd93a8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2493c475e4a94bf60a91",
+    "url": "/quiz-app-with-react/static/js/main.b2fce88a.chunk.js"
+  },
+  {
+    "revision": "522869e886438f93a865",
+    "url": "/quiz-app-with-react/static/js/runtime-main.ae87f36a.js"
+  }
+]);
